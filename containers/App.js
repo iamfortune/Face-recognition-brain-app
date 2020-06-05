1c9b0e2656a8b8517b123c43f1cf8a6f5700f64e
iamfortune <ikechifortune@gmail.com>
2020-06-05 11:27:17 +0100

    added the app.js into container

--- containers/App.js
+++ containers/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import Particles from "react-particles-js";
 import Clarifai from "clarifai";
-import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
-import Navigation from "./components/Navigation/Navigation";
-import Signin from "./components/Signin/Signin";
-import Register from "./components/Register/Register";
-import Logo from "./components/Logo/Logo";
-import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
-import Rank from "./components/Rank/Rank";
+import FaceRecognition from "../src/components/FaceRecognition/FaceRecognition";
+import Navigation from "../src/components/Navigation/Navigation";
+import Signin from "../src/components/Signin/Signin";
+import Register from "../src/components/Register/Register";
+import Logo from "../src/components/Logo/Logo";
+import ImageLinkForm from "../src/components/ImageLinkForm/ImageLinkForm";
+import Rank from "../src/components/Rank/Rank";
 import "./App.css";
 
 //You must add your own API key here from Clarifai.
