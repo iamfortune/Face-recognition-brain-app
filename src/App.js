7a884baf87267a667db861db96527bd6c811cf9c
iamfortune <ikechifortune@gmail.com>
2020-06-05 10:54:02 +0100

    improved the error handling by adding a .catch

--- src/App.js
+++ src/App.js
@@ -98,7 +98,8 @@ class App extends Component {
             .then((response) => response.json())
             .then((count) => {
               this.setState(Object.assign(this.state.user, { entries: count }));
-            });
+            })
+            .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
